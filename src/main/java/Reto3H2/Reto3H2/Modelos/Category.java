93b14c0f7bdb7a8703d041a85ad5253e5209d935
Neslop <Administrador@ADM-GOG-001.occi.clinica.com>
2021-10-29 10:20:16 -0500

    Actualizacion 1 Back Reto5

--- src/main/java/Reto3H2/Reto3H2/Modelos/Category.java
+++ src/main/java/Reto3H2/Reto3H2/Modelos/Category.java
@@ -27,7 +27,7 @@ public class Category implements Serializable{
     private String description;
     
     @OneToMany(cascade = {CascadeType.PERSIST},mappedBy="category")
-    @JsonIgnoreProperties("category")
+    @JsonIgnoreProperties({"category","message"})
     private List<Game> games;
 
     public Integer getId() {
